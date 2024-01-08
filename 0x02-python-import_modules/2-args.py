399ec9d3504110bba57649f8365be936900e3ad5
Obrabigfish <obrabigfish1@gmail.com>
2024-01-08 01:16:43 -0800

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    total = len(argv) - 1
+    if total < 1:
+        print("{} arguments.".format(total))
+    elif total == 1:
+        print("{} argument:".format(total))
+    else:
+        print("{} arguments:".format(total))
+    for i in range(total):
+            print("{}: {}".format(i + 1, argv[i +1])) 
